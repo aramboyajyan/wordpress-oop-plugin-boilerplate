c1a6e26689e957fd9f91839a28b98e73d3c4882e
Topsitemakers <support@topsitemakers.com>
2013-03-18 12:07:22 +0100

    Ordering pages according to the title

--- includes/helper.common.php
+++ includes/helper.common.php
@@ -214,7 +214,7 @@ endif;
 if (!function_exists('boilerplate_get_list_of_posts')):
 function boilerplate_get_list_of_posts($type = 'post') {
   global $wpdb;
-  $posts_results = $wpdb->get_results($wpdb->prepare("SELECT `ID`, `post_title` FROM $wpdb->posts WHERE `post_type` = '%s' AND `post_status` = 'publish'", array($type)));
+  $posts_results = $wpdb->get_results($wpdb->prepare("SELECT `ID`, `post_title` FROM $wpdb->posts WHERE `post_type` = '%s' AND `post_status` = 'publish' ORDER BY `post_title` ASC", array($type)));
   $posts = array();
   foreach ($posts_results as $post) {
     $posts[$post->ID] = $post->post_title . ' (ID: ' . $post->ID . ')';
