0f37b442a8ff02a802cc38d8a06c49b900f45313
Topsitemakers <support@topsitemakers.com>
2013-03-21 18:07:00 +0100

    Printing instead of returning the message for no items

--- includes/class.user-list.php
+++ includes/class.user-list.php
@@ -71,7 +71,7 @@ class Boilerplate_List_Users extends WP_List_Table {
    * Message displayed to the admin when there are no results.
    */
   public function no_items() {
-    return __('There are no logs in the database at the moment.');
+    _e('There are no logs in the database at the moment.');
   }
 
   /**
